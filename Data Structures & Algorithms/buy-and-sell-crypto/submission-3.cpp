e113658b090daccf68ff46839f45efb769cbba9e
saumyamudgal30-cyber <saumyamudgal30@gmail.com>
2026-08-07 12:56:43 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l =0;
+        int r= 1;
+        int maxp = 0;
+        while (r<prices.size()){
+            if(prices[l]<prices[r]){
+                int profit = prices[r] - prices[l];
+                maxp = max(profit ,maxp);
+            }
+            else{
+                l=r;
+            }
+            r++;
+        }
+        return maxp;
+
+    }
+
+};
