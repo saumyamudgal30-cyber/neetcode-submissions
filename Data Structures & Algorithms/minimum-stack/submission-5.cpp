ba879f5077c281706fc498f42531b787cf4e2359
saumyamudgal30-cyber <saumyamudgal30@gmail.com>
2026-08-15 22:38:51 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.cpp
@@ -0,0 +1,34 @@
+class MinStack {
+    std::stack<int> stack;
+    std::stack<int> minStack;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stack.push(val);
+        if(minStack.empty() || val<=minStack.top())
+        minStack.push(val);
+        else
+        minStack.push(minStack.top());//Duplicating the minimum ensures that stack and minStack stay identical in size
+
+        
+        
+    }
+    
+    void pop() {
+        stack.pop();
+        minStack.pop();
+        
+    }
+    
+    int top() {
+        return stack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+        
+    }
+};
